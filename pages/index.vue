b82f593cd80a733e955e57b35d5f3b0c7d88880b
darivs <dtack192@gmail.com>
2019-11-09 16:46:45 +0100

    Redirect all non-existing routes to index

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,16 @@
 <template>
-  <v-layout> </v-layout>
+  <v-row align="center" justify="center">
+    <v-btn block x-large class="headline" color="#ff8800">
+      <v-img
+        class="mx-0 px-0"
+        contain
+        max-height="35"
+        max-width="80"
+        :src="require('~/assets/sc_white.png')"
+      />
+      <span>Soundcloud</span>
+    </v-btn>
+  </v-row>
 </template>
 
 <script>
